5e72142b8b0c2f1e82203bfa2ff3c684ebe3e63f
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2022-07-05 02:03:48 +0500

    Location fetching completed

--- app/src/main/java/com/example/bustracking/Home.kt
+++ app/src/main/java/com/example/bustracking/Home.kt
@@ -1,14 +1,32 @@
 package com.example.bustracking
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.bustracking.databinding.ActivityHomeBinding
+import com.google.android.gms.location.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class Home : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
@@ -16,9 +34,9 @@ class Home : AppCompatActivity() {
     private var sharingLocation = false
     private lateinit var mDatabase: FirebaseDatabase
 
-//    private late init var mFusedLocationClient: FusedLocationProviderClient
-//    private var mLatitude: Double = 0.0 // A variable which will hold the latitude value.
-//    private var mLongitude: Double = 0.0
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private var mLatitude: Double = 0.0 // A variable which will hold the latitude value.
+    private var mLongitude: Double = 0.0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,8 +53,8 @@ class Home : AppCompatActivity() {
 
         checkLocationSharing()
 
-//        //! location listener attachment.
-//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        //! location listener attachment.
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
 
         binding.btnShareLocation.setOnClickListener {
@@ -49,134 +67,136 @@ class Home : AppCompatActivity() {
 
 
 
-//    //! Get Current Location
-//    private fun getCurrentLocation() {
-//        if (!isLocationEnabled()) {
-//            Toast.makeText(
-//                this,
-//                "Your location provider is turned off. Please turn it on.",
-//                Toast.LENGTH_SHORT
-//            ).show()
-//        } else {
-//            // For Getting current location of user please have a look at below link for better understanding
-//            // https://www.androdocs.com/kotlin/getting-current-location-latitude-longitude-in-android-using-kotlin.html
-//            Dexter.withActivity(this)
-//                .withPermissions(
-//                    Manifest.permission.ACCESS_FINE_LOCATION,
-//                    Manifest.permission.ACCESS_COARSE_LOCATION
-//                )
-//                .withListener(object : MultiplePermissionsListener {
-//                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-//                        if (report!!.areAllPermissionsGranted()) {
+
+
+    //! Get Current Location
+    private fun getCurrentLocation() {
+        if (!isLocationEnabled()) {
+            Toast.makeText(
+                this,
+                "Your location provider is turned off. Please turn it on.",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            // For Getting current location of user please have a look at below link for better understanding
+            // https://www.androdocs.com/kotlin/getting-current-location-latitude-longitude-in-android-using-kotlin.html
+            Dexter.withActivity(this)
+                .withPermissions(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if (report!!.areAllPermissionsGranted()) {
+
+                            requestNewLocationData()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread()
+                .check()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun requestNewLocationData() {
+
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location? = locationResult.lastLocation
+            if (mLastLocation != null) {
+                mLatitude = mLastLocation.latitude
+            }
+            Log.e("Current Latitude", "$mLatitude")
+            if (mLastLocation != null) {
+                mLongitude = mLastLocation.longitude
+            }
+            Log.e("Current Longitude", "$mLongitude")
+
+//            // TODO(Step 2: Call the AsyncTask class fot getting an address from the latitude and longitude.)
+//            // START
+//            val addressTask =
+//                GetAddressFromLatLng(this@Home, mLatitude, mLongitude)
 //
-//                            requestNewLocationData()
-//                        }
-//                    }
-//
-//                    override fun onPermissionRationaleShouldBeShown(
-//                        permissions: MutableList<PermissionRequest>?,
-//                        token: PermissionToken?
-//                    ) {
-//                        showRationalDialogForPermissions()
-//                    }
-//                }).onSameThread()
-//                .check()
-//        }
-//    }
-//
-//    private fun isLocationEnabled(): Boolean {
-//        val locationManager: LocationManager =
-//            getSystemService(Context.LOCATION_SERVICE) as LocationManager
-//        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-//            LocationManager.NETWORK_PROVIDER
-//        )
-//    }
-//
-//    private fun requestNewLocationData() {
-//
-//        val mLocationRequest = LocationRequest()
-//        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-//        mLocationRequest.interval = 0
-//        mLocationRequest.fastestInterval = 0
-//        mLocationRequest.numUpdates = 1
-//
-//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-//        if (ActivityCompat.checkSelfPermission(
-//                this,
-//                Manifest.permission.ACCESS_FINE_LOCATION
-//            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-//                this,
-//                Manifest.permission.ACCESS_COARSE_LOCATION
-//            ) != PackageManager.PERMISSION_GRANTED
-//        ) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//            //                                          int[] grantResults)
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//            return
-//        }
-//        mFusedLocationClient.requestLocationUpdates(
-//            mLocationRequest, mLocationCallback,
-//            Looper.myLooper()
-//        )
-//    }
-//    private val mLocationCallback = object : LocationCallback() {
-//        override fun onLocationResult(locationResult: LocationResult) {
-//            val mLastLocation: Location? = locationResult.lastLocation
-//            if (mLastLocation != null) {
-//                mLatitude = mLastLocation.latitude
-//            }
-//            Log.e("Current Latitude", "$mLatitude")
-//            if (mLastLocation != null) {
-//                mLongitude = mLastLocation.longitude
-//            }
-//            Log.e("Current Longitude", "$mLongitude")
-//
-////            // TODO(Step 2: Call the AsyncTask class fot getting an address from the latitude and longitude.)
-////            // START
-////            val addressTask =
-////                GetAddressFromLatLng(this@Home, mLatitude, mLongitude)
-////
-////            addressTask.setAddressListener(object :
-////                GetAddressFromLatLng.AddressListener {
-////                override fun onAddressFound(address: String?) {
-////                    Log.e("Address ::", "" + address)
-////                    //?et_location.setText(address) // Address is set to the edittext
-////                }
-////
-////                override fun onError() {
-////                    Log.e("Get Address ::", "Something is wrong...")
-////                }
-////            })
-////
-////            addressTask.getAddress()
-//            // END
-//        }
-//    }
-//
-//    private fun showRationalDialogForPermissions() {
-//        AlertDialog.Builder(this)
-//            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
-//            .setPositiveButton(
-//                "GO TO SETTINGS"
-//            ) { _, _ ->
-//                try {
-//                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-//                    val uri = Uri.fromParts("package", packageName, null)
-//                    intent.data = uri
-//                    startActivity(intent)
-//                } catch (e: ActivityNotFoundException) {
-//                    e.printStackTrace()
+//            addressTask.setAddressListener(object :
+//                GetAddressFromLatLng.AddressListener {
+//                override fun onAddressFound(address: String?) {
+//                    Log.e("Address ::", "" + address)
+//                    //?et_location.setText(address) // Address is set to the edittext
 //                }
-//            }
-//            .setNegativeButton("Cancel") { dialog,
-//                                           _ ->
-//                dialog.dismiss()
-//            }.show()
-//    }
+//
+//                override fun onError() {
+//                    Log.e("Get Address ::", "Something is wrong...")
+//                }
+//            })
+//
+//            addressTask.getAddress()
+            // END
+        }
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog,
+                                           _ ->
+                dialog.dismiss()
+            }.show()
+    }
 
 
 
