4f6ab494632e6cc833b0182cbb7bb3ada859e1b0
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2022-09-22 19:54:53 +0500

    Admin Activity Updated (Added Drivers list)

--- app/src/main/java/com/example/bustracking/Admin.kt
+++ app/src/main/java/com/example/bustracking/Admin.kt
@@ -4,14 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.bustracking.adapters.RecyclerAdapter
 import com.example.bustracking.databinding.ActivityAdminBinding
+import com.example.bustracking.modals.RVBusDriverModal
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
-class Admin : AppCompatActivity() {
+class Admin : AppCompatActivity(), RecyclerAdapter.RecyclerAdapterInterface{
     private lateinit var binding: ActivityAdminBinding
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
+
+    private lateinit var rvBusDriverModalArrayList: ArrayList<RVBusDriverModal>
+    private lateinit var rvAdapter: RecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,15 +29,69 @@ class Admin : AppCompatActivity() {
         binding = ActivityAdminBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        mAuth = FirebaseAuth.getInstance()
+        //! array initialization
+        rvBusDriverModalArrayList = ArrayList()
 
+        //! firebase initializations.
+        mAuth = FirebaseAuth.getInstance()
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.getReference("Locations")
+
+        //! recycler View for bus drivers
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        rvAdapter = RecyclerAdapter(rvBusDriverModalArrayList, this)
+        binding.recyclerView.adapter = rvAdapter
+
+        //! getting all bus drivers
+        getAllBusDrivers()
+
+        //! button update scheldule
         binding.btnUpdateSchedule.setOnClickListener {
             startActivity(Intent(this, UpdateSchedule::class.java))
         }
 
+        //! button show complaint box
         binding.btnShowComplaintBox.setOnClickListener {
             // Do nothing.
         }
+    }
+
+    private fun getAllBusDrivers() {
+        rvBusDriverModalArrayList.clear()
+        databaseReference.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                snapshot.getValue(RVBusDriverModal::class.java)
+                    ?.let { rvBusDriverModalArrayList.add(it) }
+                binding.progressBar.visibility = View.GONE
+                rvAdapter.notifyDataSetChanged()
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                snapshot.getValue(RVBusDriverModal::class.java)
+                    ?.let { rvBusDriverModalArrayList.add(it) }
+                binding.progressBar.visibility = View.GONE
+                rvAdapter.notifyDataSetChanged()
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                snapshot.getValue(RVBusDriverModal::class.java)
+                    ?.let { rvBusDriverModalArrayList.add(it) }
+                binding.progressBar.visibility = View.GONE
+                rvAdapter.notifyDataSetChanged()
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                snapshot.getValue(RVBusDriverModal::class.java)
+                    ?.let { rvBusDriverModalArrayList.add(it) }
+                binding.progressBar.visibility = View.GONE
+                rvAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -43,5 +107,11 @@ class Admin : AppCompatActivity() {
             finish()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBusClick(position: Int) {
+        val intent = Intent(this, MapsActivity::class.java)
+        intent.putExtra("busName", rvBusDriverModalArrayList[position].userName)
+        startActivity(intent)
     }
 }
